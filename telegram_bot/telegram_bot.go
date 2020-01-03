ac57e69e07c2db94c7514b66da6af303b5585020
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-01-03 17:48:25 +0100

    Telegram bot now accepting on/off messages

--- telegram_bot/telegram_bot.go
+++ telegram_bot/telegram_bot.go
@@ -2,18 +2,26 @@ package telegram_bot
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Alberto-Izquierdo/RPIHomeServer-go/configuration_loader"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func LaunchTelegramBot(config configuration_loader.TelegramBotConfiguration, outputChannel, inputChannel chan string, exitChannel chan bool) (err error) {
-	bot, err := tgbotapi.NewBotAPI(config.TelegramBotToken)
+func LaunchTelegramBot(config configuration_loader.InitialConfiguration, outputChannel chan configuration_loader.Action, inputChannel chan string, exitChannel chan bool) (err error) {
+	bot, err := tgbotapi.NewBotAPI(config.TelegramBotConfiguration.TelegramBotToken)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	fmt.Println("Telegram bot created correctly, waiting for messages")
+
+	actionsMap := make(map[string]messageHandlingFunc)
+	actionsMap["start"] = getMessagesAvailableMarkup
+	for _, pin := range config.PinsActive {
+		actionsMap["turn"+pin.Name+"On"] = turnPinOn
+		actionsMap["turn"+pin.Name+"Off"] = turnPinOff
+	}
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -35,17 +43,22 @@ func LaunchTelegramBot(config configuration_loader.TelegramBotConfiguration, out
 				}
 			}
 			userAuthorized := false
-			for _, user := range config.AuthorizedUsers {
+			for _, user := range config.TelegramBotConfiguration.AuthorizedUsers {
 				if user == update.Message.From.ID {
 					userAuthorized = true
 				}
 			}
 			if userAuthorized {
-				outputChannel <- update.Message.Text
-				response := <-inputChannel
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
-				msg.ReplyToMessageID = update.Message.MessageID
-				bot.Send(msg)
+				key := strings.Fields(update.Message.Text)[0]
+				if actionFunction, ok := actionsMap[key]; !ok {
+					fmt.Println("Action not available")
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Action not available")
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+				} else {
+					msg := actionFunction(update.Message.Text, config, update.Message.Chat.ID, update.Message.MessageID, outputChannel, inputChannel)
+					bot.Send(msg)
+				}
 			} else {
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "User not authorized :(")
 				msg.ReplyToMessageID = update.Message.MessageID
@@ -54,4 +67,41 @@ func LaunchTelegramBot(config configuration_loader.TelegramBotConfiguration, out
 		}
 	}
 	return nil
+}
+
+func getMessagesAvailableMarkup(_ string, config configuration_loader.InitialConfiguration, ChatID int64, ReplyToMessageID int, outputChannel chan configuration_loader.Action, inputChannel chan string) tgbotapi.MessageConfig {
+	markup := tgbotapi.InlineKeyboardMarkup{
+		InlineKeyboard: [][]tgbotapi.InlineKeyboardButton{
+			[]tgbotapi.InlineKeyboardButton{
+				// TODO: fill with pins
+				tgbotapi.InlineKeyboardButton{Text: "test"},
+			},
+		},
+	}
+	edit := tgbotapi.NewEditMessageReplyMarkup(ChatID, ReplyToMessageID, markup)
+	msg := tgbotapi.NewMessage(ChatID, "Action not available")
+	msg.ReplyToMessageID = ReplyToMessageID
+	msg.ReplyMarkup = edit
+	return msg
+}
+
+func turnPinOn(message string, config configuration_loader.InitialConfiguration, ChatID int64, ReplyToMessageID int, outputChannel chan configuration_loader.Action, inputChannel chan string) tgbotapi.MessageConfig {
+	pin := strings.Fields(message)[0]
+	outputChannel <- configuration_loader.Action{pin, true}
+	return processGPIOResponse(inputChannel, ChatID, ReplyToMessageID)
+}
+
+func turnPinOff(message string, config configuration_loader.InitialConfiguration, ChatID int64, ReplyToMessageID int, outputChannel chan configuration_loader.Action, inputChannel chan string) tgbotapi.MessageConfig {
+	pin := strings.Fields(message)[0]
+	outputChannel <- configuration_loader.Action{pin, false}
+	return processGPIOResponse(inputChannel, ChatID, ReplyToMessageID)
+}
+
+type messageHandlingFunc func(action string, config configuration_loader.InitialConfiguration, ChatID int64, ReplyToMessageID int, outputChannel chan configuration_loader.Action, inputChannel chan string) tgbotapi.MessageConfig
+
+func processGPIOResponse(inputChannel chan string, chatId int64, replyToMessageId int) tgbotapi.MessageConfig {
+	response := <-inputChannel
+	msg := tgbotapi.NewMessage(chatId, response)
+	msg.ReplyToMessageID = replyToMessageId
+	return msg
 }
--- telegram_bot/telegram_bot_test.go
+++ telegram_bot/telegram_bot_test.go
@@ -9,11 +9,13 @@ import (
 
 func TestLaunchTelegramBot(t *testing.T) {
 	telegramInputChannel := make(chan string)
-	telegramOutputChannel := make(chan string)
+	telegramOutputChannel := make(chan configuration_loader.Action)
 	telegramExitChannel := make(chan bool)
-	var config configuration_loader.TelegramBotConfiguration
-	config.TelegramBotToken = "153667468:AAHlSHlMqSt1f_uFmVRJbm5gntu2HI4WW8I"
-	config.AuthorizedUsers = append(config.AuthorizedUsers, 1234, 5678)
+	var config configuration_loader.InitialConfiguration
+	var telegramConfig configuration_loader.TelegramBotConfiguration
+	config.TelegramBotConfiguration = &telegramConfig
+	config.TelegramBotConfiguration.TelegramBotToken = "153667468:AAHlSHlMqSt1f_uFmVRJbm5gntu2HI4WW8I"
+	config.TelegramBotConfiguration.AuthorizedUsers = append(config.TelegramBotConfiguration.AuthorizedUsers, 1234, 5678)
 	go func() {
 		time.Sleep(2 * time.Second)
 		close(telegramExitChannel)
