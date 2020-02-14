b933b9d18a6a59b684d1ad1e1436c7732f63f141
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-02-14 17:18:18 +0100

    Refactor in message_generator: function separation and new data structure

--- message_generator/message_generator.go
+++ message_generator/message_generator.go
@@ -9,8 +9,50 @@ import (
 	"github.com/Alberto-Izquierdo/RPIHomeServer-go/configuration_loader"
 )
 
+type ProgrammedAction struct {
+	Action configuration_loader.ActionTime
+	Repeat bool
+}
+
+func (this ProgrammedAction) LessThan(other interface{}) bool {
+	return time.Time(this.Action.Time).Before(time.Time(other.(ProgrammedAction).Action.Time))
+}
+
 func Run(actions []configuration_loader.ActionTime, outputChannel chan configuration_loader.Action, exitChannel chan bool) error {
 	queue := ordered_queue.OrderedQueue{}
+	err := initQueue(actions, &queue)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	go func() {
+		for {
+			nextElement, err := queue.Pop()
+			now := time.Now()
+			t := now.Add(time.Hour * 10000)
+			var nextAction ProgrammedAction
+
+			if err != nil {
+				fmt.Println("[message_generator]: Error when trying to pop elements from actions queue: ", err.Error(), "")
+			} else {
+				nextAction = nextElement.(ProgrammedAction)
+				t = time.Time(nextAction.Action.Time)
+			}
+
+			select {
+			case _ = <-exitChannel:
+				fmt.Println("[message_generator] Exit signal received, exiting...")
+				exitChannel <- true
+				return
+			case <-time.After(t.Sub(now)):
+				handleNextAction(&nextAction, outputChannel, &queue, exitChannel)
+			}
+		}
+	}()
+	return nil
+}
+
+func initQueue(actions []configuration_loader.ActionTime, queue *ordered_queue.OrderedQueue) error {
 	if len(actions) == 0 {
 		return errors.New("No actions to launch")
 	}
@@ -22,42 +64,30 @@ func Run(actions []configuration_loader.ActionTime, outputChannel chan configura
 			date = date.Add(time.Hour * 24)
 		}
 		actionTime.Time = configuration_loader.MyTime(date)
-		err := queue.Push(actionTime)
+		err := queue.Push(ProgrammedAction{actionTime, true})
 		if err != nil {
 			return errors.New("[message_generator]: Could not push elements into the queue: " + err.Error())
 		}
 	}
-	go func() {
-		for {
-			nextElement, err := queue.Pop()
-			if err != nil {
-				fmt.Println("[message_generator]: Error when trying to pop elements from actions queue: ", err.Error())
-				exitChannel <- true
-				return
-			}
-			nextAction := nextElement.(configuration_loader.ActionTime)
-			t := time.Time(nextAction.Time)
-			now := time.Now()
-
-			select {
-			case _ = <-exitChannel:
-				fmt.Println("[message_generator] Exit signal received, exiting...")
-				exitChannel <- true
-				return
-			case <-time.After(t.Sub(now)):
-				// Enqueue the action to the gpio manager
-				outputChannel <- nextAction.Action
-				// Push the action again but with the time increased 24 hours
-				newTime := now.Add(time.Hour * 24)
-				newAction := configuration_loader.ActionTime{nextAction.Action, configuration_loader.MyTime(newTime)}
-				err := queue.Push(newAction)
-				if err != nil {
-					fmt.Println("[message_generator]: Could not push elements into the queue: ", err.Error())
-					exitChannel <- true
-					return
-				}
-			}
-		}
-	}()
 	return nil
+}
+
+func handleNextAction(nextAction *ProgrammedAction, outputChannel chan configuration_loader.Action, queue *ordered_queue.OrderedQueue, exitChannel chan bool) {
+	// Enqueue the action to the gpio manager
+	outputChannel <- nextAction.Action.Action
+	// Push the action again but with the time increased 24 hours
+	if nextAction.Repeat {
+		newTime := time.Now().Add(time.Hour * 24)
+		newAction := ProgrammedAction{
+			configuration_loader.ActionTime{
+				nextAction.Action.Action,
+				configuration_loader.MyTime(newTime)},
+			true}
+		err := queue.Push(newAction)
+		if err != nil {
+			fmt.Println("[message_generator]: Could not push elements into the queue: ", err.Error())
+			exitChannel <- true
+			return
+		}
+	}
 }
